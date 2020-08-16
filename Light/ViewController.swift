a8e8e9c36a8db73adca1386865a2540246c37f31
Lindsey <lindsey@blacktopinteractive.com>
2020-08-16 12:45:29 -0500

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Lindsey Myrick on 8/16/20.
+//  Copyright © 2020 Lindsey Myrick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
